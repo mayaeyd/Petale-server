76514b0d824285ad295ed32b00230721a464af8c
Maya Eid <mayabaseleid@gmail.com>
2024-12-13 01:28:08 +0200

    backend: Connect to database in server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectToDB from "./db/connection.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(
   })
 );
 
-app.listen(process.env.SERVER_PORT,()=>{
-    console.log(`Server running on port ${process.env.SERVER_PORT}`);
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server running on port ${process.env.SERVER_PORT}`);
+  connectToDB();
 });
