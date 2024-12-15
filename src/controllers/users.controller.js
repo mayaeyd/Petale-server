b66ac99b6ce6e21d6d8b1f0a80d1ebd0938564fd
Maya Eid <mayabaseleid@gmail.com>
2024-12-15 19:25:24 +0200

    backend: Create a banUser API

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -17,3 +17,32 @@ export const getUsers = async (req, res) => {
     return res.status(500).send({ message: "Server error" });
   }
 };
+
+export const banUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).send({ message: "User id was not provided" });
+    }
+
+    const bannedUser = await User.findByIdAndUpdate(
+      id,
+      { isBanned: true },
+      { new: true }
+    );
+
+    if(!bannedUser){
+        return res.status(404).send({message:"User not found"});
+    }
+
+    return res.status(200).json({
+        success: true,
+        message: "User has been banned successfully",
+        user: bannedUser,
+    });
+
+  } catch (error) {
+    console.error("Error banning user:", error.message);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
