a0ad0aea97d25fcad53740024ebf2588a272aec6
Maya Eid <mayabaseleid@gmail.com>
2024-12-15 20:16:27 +0200

    backend: Set up authMiddleware and get token from authorization header if user is authorized

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/users.model.js';
+
+export const authMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({
+      message: "Unauthorized",
+    });
+  }
+
+  const splitted = authHeader.split(" ");
+
+  if (splitted.length !== 2 || splitted[0] !== "Bearer") {
+    return res.status(401).send({
+      message: "Unauthorized",
+    });
+  }
+
+  const token = splitted[1];
+};
