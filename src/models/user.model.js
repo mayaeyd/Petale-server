1aaa258e4ec21406fc21f07c4b40887124c96b87
Maya Eid <mayabaseleid@gmail.com>
2024-12-15 15:53:27 +0200

    backend: Set up User schema

---
+++ src/models/user.model.js
@@ -0,0 +1,156 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user", "gardener"],
+      default: "user",
+    },
+    gardenerProfile: {
+      garden: {
+        name: {
+          type: String,
+          default: null,
+        },
+        location: {
+          type: String,
+          default: null,
+        },
+        plants: [
+          {
+            plantType: {
+              type: String,
+              enum: ["plant", "flower"],
+              required: true,
+            },
+            scientificName: {
+              type: String,
+              required: true,
+            },
+            plantedDate: {
+              type: Date,
+              required: true,
+            },
+            sensorData: {
+              currentMoisture: {
+                type: Number,
+                required: true,
+              },
+              currentHumidity: {
+                type: Number,
+                required: true,
+              },
+              currentTemperature: {
+                type: Number,
+                required: true,
+              },
+              lastUpdated: {
+                type: Date,
+                default: Date.now,
+              },
+            },
+            wateringSchedule: {
+              frequency: {
+                type: String,
+                enum: ["daily", "weekly"],
+                default: "weekly",
+              },
+              nextWateringTime: {
+                type: Date,
+                default: Date.now,
+              },
+              isAutomatedWatering: {
+                type: Boolean,
+                default: false,
+              },
+            },
+          },
+        ],
+      },
+      marketplaceListings: [
+        {
+          plantName: {
+            type: String,
+            required: true,
+          },
+          harvestDate: {
+            type: Date,
+            required: true,
+          },
+          price: {
+            type: Number,
+            required: true,
+          },
+          description: {
+            type: String,
+            required: true,
+          },
+          quantity: {
+            type: Number,
+            required: true,
+          },
+          images: {
+            type: [String],
+            default: [],
+          },
+          status: {
+            type: String,
+            enum: ["available", "sold"],
+            default: "available",
+          },
+        },
+      ],
+      default: null,
+    },
+
+    purchaseHistory: [
+      {
+        listingId: { type: mongoose.Schema.Types.ObjectId, required: true },
+        purchaseDate: {
+          type: Date,
+          default: Date.now,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        totalPrice: {
+          type: Number,
+          required: true,
+        },
+        sellerGardenerId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
