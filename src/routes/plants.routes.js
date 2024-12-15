2505f18dc39d000c7af0db3f9b1bddc94919ab08
Maya Eid <mayabaseleid@gmail.com>
2024-12-15 23:01:29 +0200

    backend: Implement remaining CRUD routes for plant management

--- src/routes/plants.routes.js
+++ src/routes/plants.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { gardenerMiddleware } from "../middlewares/gardener.middleware.js";
-import { addPlant } from "../controllers/plants.controller.js";
+import { addPlant, getPlant, editPlant, deletePlant } from "../controllers/plants.controller.js";
 
 const router = new Router();
 
-router.post("/addPlant", authMiddleware, gardenerMiddleware, addPlant);
+router.get("/:id?",authMiddleware, gardenerMiddleware, getPlant)
+router.post("/", authMiddleware, gardenerMiddleware, addPlant);
+router.put("/:id",authMiddleware, gardenerMiddleware, editPlant);
+router.delete("/:id",authMiddleware, gardenerMiddleware, deletePlant);
 
 export default router;
