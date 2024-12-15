ff8bc630adef86221b4facf2e9d2e73c5100d864
Maya Eid <mayabaseleid@gmail.com>
2024-12-15 20:07:52 +0200

    backend: Create admin middleware

---
+++ src/middlewares/admin.middleware.js
@@ -0,0 +1,10 @@
+export const adminMiddleware = async (req, res, next) => {
+    if (req.user.role === "admin") {
+      next();
+    } else {
+      res.status(401).send({
+        message: "Unauthorized",
+      });
+    }
+  };
+  
