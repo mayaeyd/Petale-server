5d9c4583687c223c93f412a8d5c634b46b172d06
Maya Eid <mayabaseleid@gmail.com>
2024-12-15 21:46:52 +0200

    backend: Add authMiddleware to access getSelf API

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -14,6 +14,6 @@ router.post("/login", login);
 router.post("/register", register);
 router.put("/updateProfile/:id", authMiddleware, updateProfile);
 router.post("/login/admin", adminLogin);
-router.post("/getSelf", getSelf);
+router.post("/getSelf", authMiddleware, getSelf);
 
 export default router;
