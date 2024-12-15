2fb6e4bb1c67bb754bad8cd47116e2331cb75e55
Maya Eid <mayabaseleid@gmail.com>
2024-12-15 21:45:08 +0200

    backend: Optimize admin middleware

--- src/middlewares/admin.middleware.js
+++ src/middlewares/admin.middleware.js
@@ -1,10 +1,8 @@
 export const adminMiddleware = async (req, res, next) => {
-    if (req.user.role === "admin") {
-      next();
-    } else {
-      res.status(401).send({
-        message: "Unauthorized",
-      });
-    }
-  };
-  
+  if (!req.user || req.user.role !== "admin") {
+    return res.status(401).send({
+      message: "Unauthorized",
+    });
+  }
+  next();
+};
