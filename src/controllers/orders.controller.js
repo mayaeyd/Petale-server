9c39df30b4b7e3aac2b6b740a2acad82832c5afd
Maya Eid <mayabaseleid@gmail.com>
2024-12-16 14:18:22 +0200

    backend: Initialize order management APIs with endpoints for getting, creating, canceling and tracking an order

---
+++ src/controllers/orders.controller.js
@@ -0,0 +1,9 @@
+import User from "../models/user.model.js";
+
+export const getOrders = async (req, res) => {};
+
+export const createOrder = async (req, res) => {};
+
+export const cancelOrder = async (req, res) => {};
+
+export const trackOrder = async (req, res) => {};
