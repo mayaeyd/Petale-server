10d1f565809680ffb2127e1e9d912453d19c897e
Maya Eid <mayabaseleid@gmail.com>
2024-12-17 22:15:01 +0200

    backend: Add basic user and order validation logic in getOrders API

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -1,6 +1,18 @@
 import User from "../models/user.model.js";
 
-export const getOrders = async (req, res) => {};
+export const getOrders = async (req, res) => {
+  const orderId = req.params.id;
+  const userId = req.user._id;
+
+  if (!userId) {
+    return res.status(400).send({ message: "User ID is required" });
+  }
+
+  const user = await User.findById(userId);
+  if (!user) {
+    return res.status(404).send({ message: "User not found" });
+  }
+};
 
 export const createOrder = async (req, res) => {};
 
