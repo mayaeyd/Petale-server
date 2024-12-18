0c002e3ee05e9ad816f6cd462455f584c8df1234
Maya Eid <mayabaseleid@gmail.com>
2024-12-18 19:12:07 +0200

    backend: Add validation for required fields in postPlant API

--- src/controllers/plants.controller.js
+++ src/controllers/plants.controller.js
@@ -166,13 +166,17 @@ export const postPlant = async (req, res) => {
   const plantId = req.params.id;
   const userId = req.user._id;
 
-  const {plantName, harvestDate, price, description, quantity, images} = req.body;
-
   try {
     const user = await User.findById(userId);
     if (!user) {
       return res.status(404).send({ message: "User not found" });
     }
+
+    const {plantName, harvestDate, price, description, quantity, images} = req.body;
+    if(!plantName || !harvestDate || !price || !description || !quantity || !images){
+      return res.status(400).send({message:"All fields are required"});
+    }
+
     const plant = user.gardenerProfile.garden.plants.find(
       (plant) => plant._id.toString() === plantId
     );
