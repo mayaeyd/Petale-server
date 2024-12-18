9afcf2c1916e201599e019d3e2ace02819a69ea7
Maya Eid <mayabaseleid@gmail.com>
2024-12-18 11:48:09 +0200

    backend: Install and import Imagekit SDK

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import ImageKit from "imagekit";
 import connectToDB from "./db/connection.js";
 import authRoutes from "./routes/auth.routes.js";
 import usersRoutes from "./routes/users.routes.js";
