e61c980d135d21b1f9327936bb090077ca34b732
Maya Eid <mayabaseleid@gmail.com>
2024-12-19 02:32:36 +0200

    backend: Integrate authMiddleware in all order routes

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { cancelOrder, createOrder, getOrders, trackOrder } from "../controllers/orders.controller";
+import {
+  cancelOrder,
+  createOrder,
+  getOrders,
+  trackOrder,
+} from "../controllers/orders.controller";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = new Router();
 
-router.get("/:id?", getOrders);
-router.post("/",createOrder);
-router.delete("/:id", cancelOrder);
-router.get("/:id", trackOrder);
+router.get("/:id?", authMiddleware, getOrders);
+router.post("/", authMiddleware, createOrder);
+router.delete("/:id", authMiddleware, cancelOrder);
+router.get("/:id", authMiddleware, trackOrder);
