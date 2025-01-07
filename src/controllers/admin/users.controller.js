1b36ef7cd605228458be295a340ce5c259726349
Maya Eid <mayabaseleid@gmail.com>
2025-01-07 23:18:47 +0200

    backend: Get all users' info excluding plants, marketplaceListings and passwords in getAllUsers API

--- src/controllers/admin/users.controller.js
+++ src/controllers/admin/users.controller.js
@@ -1,12 +1,34 @@
 //getAllUsers
 //toggleUserBan
 
+import User from "../../models/user.model.js";
+
 export const getAllUsers = async (req, res) => {
   try {
+    const users = await User.find({}, "-password")
+      .select("-gardenerProfile.garden.plants")
+      .select("-gardenerProfile.marketplaceListings");
+
+    res.status(200).send({
+      success: true,
+      count: users.length,
+      data: users,
+    });
   } catch (error) {
     res.status(500).send({
       success: false,
       message: "Error fetching users",
+      error: error.message,
+    });
+  }
+};
+
+export const toggleUserBan = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Error updating user status",
       error: error.message,
     });
   }
