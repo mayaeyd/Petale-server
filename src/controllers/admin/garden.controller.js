2d767c49c98d89ea0f31359f903d2bf98b087e0c
Maya Eid <mayabaseleid@gmail.com>
2025-01-08 01:42:33 +0200

    backend: Create an object that stores gardener info and growing plants array for each user

--- src/controllers/admin/garden.controller.js
+++ src/controllers/admin/garden.controller.js
@@ -8,6 +8,15 @@ export const getAllGrowingPlants = async (req, res) => {
       { "gardenerProfile.garden.plants": { $exists: true, $ne: [] } },
       "firstName lastName email gardenerProfile.garden"
     );
+
+    const allPlants = users.map((user) => ({
+      gardenerId: user._id,
+      gardenerName: `${user.firstName} ${user.lastName}`,
+      gardenerEmail: user.email,
+      gardenName: user.gardenerProfile.garden.name,
+      location: user.gardenerProfile.garden.location,
+      plants: user.gardenerProfile.garden.plants,
+    }));
   } catch (error) {
     res.status(500).send({
       success: false,
