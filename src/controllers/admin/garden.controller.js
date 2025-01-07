e3b20d27f0a4222fbeb25de845c76eb1992cc779
Maya Eid <mayabaseleid@gmail.com>
2025-01-08 01:41:01 +0200

    backend: Find all growing plants from users in getAllGrowingPlants API

--- src/controllers/admin/garden.controller.js
+++ src/controllers/admin/garden.controller.js
@@ -1,7 +1,13 @@
 //getAllGrowingPlants
 
+import User from "../../models/user.model.js";
+
 export const getAllGrowingPlants = async (req, res) => {
   try {
+    const users = await User.find(
+      { "gardenerProfile.garden.plants": { $exists: true, $ne: [] } },
+      "firstName lastName email gardenerProfile.garden"
+    );
   } catch (error) {
     res.status(500).send({
       success: false,
