3f14d4a892ef258544cfa34ab3ac5c21ab1b6763
Maya Eid <mayabaseleid@gmail.com>
2025-01-08 01:03:21 +0200

    backend: Fetch order by ID and populate order and gardener's info

--- src/controllers/admin/orders.controller.js
+++ src/controllers/admin/orders.controller.js
@@ -1,9 +1,21 @@
 //getAllOrders
 //getAllSales
 
+import User from "../../models/user.model.js";
+
 export const getOrders = async (req, res) => {
   try {
     const { id } = req.params;
+
+    if (id) {
+      const user = await User.findOne(
+        { "purchaseHistory._id": id },
+        "firstName lastName email purchaseHistory.$"
+      ).populate(
+        "purchaseHistory.sellerGardenerId",
+        "firstName lastName email"
+      );
+    }
   } catch (error) {
     res.status(500).send({
       success: false,
