d06bfc3433486408529db1ee4da8bfe1a47ba025
Maya Eid <mayabaseleid@gmail.com>
2025-01-07 23:04:40 +0200

    backend: Setup getAllUsers API with try-catch block

--- src/controllers/admin/users.controller.js
+++ src/controllers/admin/users.controller.js
@@ -1,2 +1,13 @@
 //getAllUsers
 //toggleUserBan
+
+export const getAllUsers = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Error fetching users",
+      error: error.message,
+    });
+  }
+};
