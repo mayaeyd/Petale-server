d0b85337dbaca7b8366927accc58e70f4ad851e6
Maya Eid <mayabaseleid@gmail.com>
2025-01-08 00:18:25 +0200

    backend: Check if id is sent in params and get required post in getPost API

--- src/controllers/admin/marketplace.controller.js
+++ src/controllers/admin/marketplace.controller.js
@@ -4,8 +4,23 @@
 
 import User from "../../models/user.model.js";
 
-export const getAllPosts = async (req, res) => {
+export const getPosts = async (req, res) => {
   try {
+    const { id } = req.params;
+
+    if (id) {
+      const user = await User.findOne(
+        { "gardenerProfile.marketplaceListings._id": id },
+        "firstName lastName email gardenerProfile.marketplaceListings.$"
+      );
+
+      if (!user) {
+        return res.status(404).send({
+          success: false,
+          message: "Post not found",
+        });
+      }
+    }
   } catch (error) {
     res.status(500).send({
       success: false,
