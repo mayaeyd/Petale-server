0eebefef8112f34955b46d9118680189f6625dbe
Maya Eid <mayabaseleid@gmail.com>
2025-01-07 22:58:44 +0200

    backend: Add ping-pong mechanism and error handling to WebSocket setup

--- src/utils/socket.io.js
+++ src/utils/socket.io.js
@@ -1,11 +1,14 @@
 import { Server } from "socket.io";
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const setupWebSocket = (server) => {
-  // Initialize a socket.io server and attach it to the provided http server
+  // Initializes a socket.io server and attach it to the provided http server
   const io = new Server(server, {
     cors: { origin: "*", methods: ["GET", "POST"] },
+    pingTimeout: 30000,
+    pingInterval: 5000,
   });
+
   // Creates a WebSocket server
   const wss = new WebSocketServer({ noServer: true });
 
@@ -21,19 +24,51 @@ const setupWebSocket = (server) => {
 
   wss.on("connection", (ws) => {
     // Listens for messages from the WebSocket client
+    console.log("Arduino connected to WebSocket");
+
+    // Setup ping-pong
+    ws.isAlive = true;
+    ws.on("pong", () => {
+      ws.isAlive = true;
+    });
+
     ws.on("message", (message) => {
       try {
         const data = JSON.parse(message);
         // Broadcasts the parsed data to all connected Socket.IO clients using the sensor_data event
+        console.log("Received data:", data);
         io.emit("sensor_data", data);
       } catch (error) {
         console.error("Error parsing message:", error);
       }
     });
+
+    ws.on("error", (error) => {
+      console.error("WebSocket error:", error);
+    });
+
+    ws.on("close", () => {
+      console.log("Arduino disconnected");
+    });
+  });
+
+  const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+      if (ws.isAlive === false) {
+        return ws.terminate();
+      }
+      ws.isAlive = false;
+      ws.ping();
+    });
+  }, 30000);
+
+  wss.on("close", () => {
+    clearInterval(interval);
   });
 
   io.on("connection", (socket) => {
     // socket.io event
+    console.log("Frontend client connected");
     socket.on("water_now", (duration) => {
       wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
@@ -52,6 +87,10 @@ const setupWebSocket = (server) => {
           client.send(JSON.stringify({ type: "update_schedule", ...schedule }));
         }
       });
+    });
+    // socket.io event
+    socket.on("disconnect", () => {
+      console.log("Frontend client disconnected");
     });
   });
 
