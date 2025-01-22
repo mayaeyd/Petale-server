ea91ab2d43c67a3f071d7ead29fe8e53e039dbd8
Maya Eid <mayabaseleid@gmail.com>
2025-01-22 22:25:14 +0200

    backend: Add middleware to order routes for authentication and role-based access control

--- src/routes/marketplace.routes.js
+++ src/routes/marketplace.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { userOrAdminMiddleware } from "../middlewares/useroradmin.middleware.js";
 import { getAllPosts } from "../controllers/marketplace.controller.js";
+import { userMiddleware } from "../middlewares/user.middleware.js";
 
 const router = new Router();
 
-router.get("/", authMiddleware, userOrAdminMiddleware, getAllPosts);
+router.get("/", authMiddleware, userMiddleware, getAllPosts);
 
 export default router;
--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { userMiddleware } from "../middlewares/user.middleware.js";
 
 import {
   createOrder,
@@ -7,12 +8,23 @@ import {
   getUserOrders,
   updateOrderStatus,
 } from "../controllers/orders.controller.js";
+import { gardenerMiddleware } from "../middlewares/gardener.middleware.js";
 
 const router = new Router();
 
-router.post("/", authMiddleware, createOrder);
-router.get("/user", authMiddleware, getUserOrders);
-router.get("/gardener/:gardenerId", authMiddleware, getGardenerOrders);
-router.patch("/user/:userId/order/:orderId", authMiddleware, updateOrderStatus);
+router.post("/", authMiddleware, userMiddleware, createOrder);
+router.get("/user", authMiddleware, userMiddleware, getUserOrders);
+router.get(
+  "/gardener/:gardenerId",
+  authMiddleware,
+  gardenerMiddleware,
+  getGardenerOrders
+);
+router.patch(
+  "/user/:userId/order/:orderId",
+  authMiddleware,
+  gardenerMiddleware,
+  updateOrderStatus
+);
 
 export default router;
